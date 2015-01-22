6bea3e9f7354646bc664d23efd364c1a6413d319
Gil Tene <gil@cloud7.com>
2015-01-21 23:34:38 -0800

    Adds start/end time stamping to interval histograms (useful for logging)

--- tools/stress/src/org/apache/cassandra/stress/util/TimingInterval.java
+++ tools/stress/src/org/apache/cassandra/stress/util/TimingInterval.java
@@ -41,8 +41,8 @@ public final class TimingInterval
     public final long rowCount;
     public final long operationCount;
 
-    final Histogram expectedTimesHistogram;
-    final Histogram actualTimesHistogram;
+    public final Histogram expectedTimesHistogram;
+    public final Histogram actualTimesHistogram;
 
 
     TimingInterval(long time)
@@ -77,6 +77,9 @@ public final class TimingInterval
         Histogram mergedExpectedTimesHistogram = new Histogram(3);
         Histogram mergedActualTimesHistogram = new Histogram(3);
 
+        long startTime = Long.MAX_VALUE;
+        long endTime = 0;
+
         long end = 0;
         long pauseStart = 0, pauseEnd = Long.MAX_VALUE;
         for (TimingInterval interval : intervals)
@@ -87,12 +90,25 @@ public final class TimingInterval
             rowCount += interval.rowCount;
             mergedExpectedTimesHistogram.add(interval.expectedTimesHistogram);
             mergedActualTimesHistogram.add(interval.actualTimesHistogram);
+
+            // track start and end time across all interval histograms:
+            startTime = Math.min(startTime, interval.expectedTimesHistogram.getStartTimeStamp());
+            startTime = Math.min(startTime, interval.actualTimesHistogram.getStartTimeStamp());
+            endTime = Math.max(endTime, interval.expectedTimesHistogram.getEndTimeStamp());
+            endTime = Math.max(endTime, interval.actualTimesHistogram.getEndTimeStamp());
+
             if (interval.pauseLength > 0)
             {
                 pauseStart = Math.max(pauseStart, interval.pauseStart);
                 pauseEnd = Math.min(pauseEnd, interval.pauseStart + interval.pauseLength);
             }
         }
+
+        mergedActualTimesHistogram.setStartTimeStamp(startTime);
+        mergedActualTimesHistogram.setEndTimeStamp(endTime);
+        mergedExpectedTimesHistogram.setStartTimeStamp(startTime);
+        mergedExpectedTimesHistogram.setEndTimeStamp(endTime);
+
         if (pauseEnd < pauseStart)
             pauseEnd = pauseStart = 0;
 
@@ -180,6 +196,14 @@ public final class TimingInterval
     public long startNanos()
     {
         return start;
+    }
+
+    public Histogram getExpectedTimesHistogram() {
+        return expectedTimesHistogram;
+    }
+
+    public Histogram getActualTimesHistogram() {
+        return actualTimesHistogram;
     }
 }
 
